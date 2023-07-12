86276b6615038db565d22afa24230266f61cfcae
haroldCoder <haroldc2005@gmail.com>
2023-07-12 17:58:35 -0500

    env variables server

--- server/src/DB/credential.js
+++ server/src/DB/credential.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = exports.DB_PORT = exports.DB_NAME = exports.DB_PASSWORD = exports.DB_USER = exports.DB_HOST = void 0;
-exports.DB_HOST = 'bwh6blkptzgz1bm5wbv1-postgresql.services.clever-cloud.com';
-exports.DB_USER = 'uhkr7xzqhiw3onqoifqg';
-exports.DB_PASSWORD = 'tc8MC1eUTZDUH900NISmfE3ACpoGKT';
-exports.DB_NAME = 'bwh6blkptzgz1bm5wbv1';
-exports.DB_PORT = 5432;
+exports.DB_HOST = process.env.DB_HOST || 'bwh6blkptzgz1bm5wbv1-postgresql.services.clever-cloud.com';
+exports.DB_USER = process.env.DB_USER || 'uhkr7xzqhiw3onqoifqg';
+exports.DB_PASSWORD = process.env.DB_PASSWORD || 'tc8MC1eUTZDUH900NISmfE3ACpoGKT';
+exports.DB_NAME = process.env.DB_NAME || 'bwh6blkptzgz1bm5wbv1';
+exports.DB_PORT = parseInt(process.env.DB_PORT || '5432', 10);
 exports.PORT = process.env.PORT || 5000;
--- server/src/DB/credential.ts
+++ server/src/DB/credential.ts
@@ -2,5 +2,5 @@ export const DB_HOST = process.env.DB_HOST || 'bwh6blkptzgz1bm5wbv1-postgresql.s
 export const DB_USER = process.env.DB_USER || 'uhkr7xzqhiw3onqoifqg'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'tc8MC1eUTZDUH900NISmfE3ACpoGKT'
 export const DB_NAME = process.env.DB_NAME || 'bwh6blkptzgz1bm5wbv1'
-export const DB_PORT = process.env.DB_PORT || 5432
+export const DB_PORT: number = parseInt(process.env.DB_PORT || '5432', 10);
 export const PORT = process.env.PORT || 5000

9f907ac3b9e90c6925c06422b94a6e32060f3cf6
haroldCoder <haroldc2005@gmail.com>
2023-07-12 17:48:17 -0500

    envirament developmen backend

--- server/src/DB/credential.ts
+++ server/src/DB/credential.ts
@@ -1,6 +1,6 @@
-export const DB_HOST = 'bwh6blkptzgz1bm5wbv1-postgresql.services.clever-cloud.com'
-export const DB_USER = 'uhkr7xzqhiw3onqoifqg'
-export const DB_PASSWORD = 'tc8MC1eUTZDUH900NISmfE3ACpoGKT'
-export const DB_NAME = 'bwh6blkptzgz1bm5wbv1'
-export const DB_PORT = 5432
+export const DB_HOST = process.env.DB_HOST || 'bwh6blkptzgz1bm5wbv1-postgresql.services.clever-cloud.com'
+export const DB_USER = process.env.DB_USER || 'uhkr7xzqhiw3onqoifqg'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'tc8MC1eUTZDUH900NISmfE3ACpoGKT'
+export const DB_NAME = process.env.DB_NAME || 'bwh6blkptzgz1bm5wbv1'
+export const DB_PORT = process.env.DB_PORT || 5432
 export const PORT = process.env.PORT || 5000

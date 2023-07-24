efadef28f88b36fde58eaa1fe309474491b07610
haroldCoder <haroldc2005@gmail.com>
2023-07-24 16:06:50 -0500

    conver data in service viewMessages

--- server/src/controllers/Messages.controllers.js
+++ server/src/controllers/Messages.controllers.js
@@ -18,20 +18,35 @@ class MessagesController extends connect_1.default {
     constructor(req, res) {
         super();
         this.ViewMessages = (user_em, user_tr) => __awaiter(this, void 0, void 0, function* () {
-            this.user_em = user_em;
-            this.user_tr = user_tr;
-            this.client.query(`SELECT content 
-        FROM messages 
-        JOIN users ON messages.Id_em = users.ID 
-        WHERE (messages.Id_tr = ${this.user_em} OR messages.Id_em = ${this.user_em}) 
-          AND (messages.Id_tr = ${this.user_tr} OR messages.Id_em = ${this.user_tr})`)
-                .then((res) => {
-                this.res.status(200).json(res.rows);
-            })
-                .catch((err) => {
+            try {
+                this.user_em = user_em;
+                this.user_tr = user_tr;
+                const queryResult = yield this.client.query(`
+              SELECT content, Id_tr, Id_em
+              FROM messages 
+              JOIN users ON messages.Id_em = users.ID 
+              WHERE (messages.Id_tr = ${this.user_em} OR messages.Id_em = ${this.user_em}) 
+                AND (messages.Id_tr = ${this.user_tr} OR messages.Id_em = ${this.user_tr})
+            `);
+                const userEmQueryResult = yield this.client.query(`
+              SELECT users.Number FROM messages JOIN users ON messages.Id_em = users.ID WHERE messages.Id_em = ${this.user_em}
+            `);
+                const number_1 = userEmQueryResult.rows.length > 0 ? userEmQueryResult.rows[0].number : null;
+                const userTrQueryResult = yield this.client.query(`
+              SELECT users.Number FROM messages JOIN users ON messages.Id_tr = users.ID WHERE messages.Id_tr = ${this.user_tr}
+            `);
+                const number_2 = userTrQueryResult.rows.length > 0 ? userTrQueryResult.rows[0].number : null;
+                const newArray = queryResult.rows.map((row) => ({
+                    number_em: number_1,
+                    number_tr: number_2,
+                    content: [...row.content],
+                }));
+                this.res.status(200).json(newArray);
+            }
+            catch (err) {
                 console.log(err);
-                this.res.status(500).send(err);
-            });
+                this.res.status(500).send('Internal Server Error');
+            }
         });
         this.SendMessage = (content, user_em, user_tr) => __awaiter(this, void 0, void 0, function* () {
             this.user_em = yield new users_contollers_1.default(this.req, this.res).getIdUserByNumber(user_em, true);
--- server/src/controllers/Messages.controllers.ts
+++ server/src/controllers/Messages.controllers.ts
@@ -16,23 +16,40 @@ class MessagesController extends ConectDB{
     }
 
     ViewMessages = async(user_em: number, user_tr: number) =>{
+        try {
+            this.user_em = user_em;
+            this.user_tr = user_tr;
+        
+            const queryResult = await this.client.query(`
+              SELECT content, Id_tr, Id_em
+              FROM messages 
+              JOIN users ON messages.Id_em = users.ID 
+              WHERE (messages.Id_tr = ${this.user_em} OR messages.Id_em = ${this.user_em}) 
+                AND (messages.Id_tr = ${this.user_tr} OR messages.Id_em = ${this.user_tr})
+            `);
+        
+            const userEmQueryResult = await this.client.query(`
+              SELECT users.Number FROM messages JOIN users ON messages.Id_em = users.ID WHERE messages.Id_em = ${this.user_em}
+            `);
+            const number_1 = userEmQueryResult.rows.length > 0 ? userEmQueryResult.rows[0].number : null;
+        
 
-        this.user_em = user_em;
-        this.user_tr = user_tr;
-
-        this.client.query(`SELECT content 
-        FROM messages 
-        JOIN users ON messages.Id_em = users.ID 
-        WHERE (messages.Id_tr = ${this.user_em} OR messages.Id_em = ${this.user_em}) 
-          AND (messages.Id_tr = ${this.user_tr} OR messages.Id_em = ${this.user_tr})`)
-        .then((res)=>{
-            this.res.status(200).json(res.rows);
-        })
-        .catch((err)=>{
+            const userTrQueryResult = await this.client.query(`
+              SELECT users.Number FROM messages JOIN users ON messages.Id_tr = users.ID WHERE messages.Id_tr = ${this.user_tr}
+            `);
+            const number_2 = userTrQueryResult.rows.length > 0 ? userTrQueryResult.rows[0].number : null;
+        
+            const newArray = queryResult.rows.map((row: any) => ({
+              number_em: number_1,
+              number_tr: number_2,
+              content: [...row.content],
+            }));
+        
+            this.res.status(200).json(newArray);
+          } catch (err) {
             console.log(err);
-            
-            this.res.status(500).send(err);
-        })
+            this.res.status(500).send('Internal Server Error');
+          }
     }
 
     SendMessage = async(content: string, user_em: string, user_tr: string) =>{
